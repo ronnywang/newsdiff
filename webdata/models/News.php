8af90fa36d6dd59881b9445b4d3490a016a90fac
Ronny Wang <ronnywang@gmail.com>
2013-08-13 21:53:28 +0800

    如果 title, body 是 0 就跳過(可能是抓取失敗), 並且更新 diff_count, 並把 diffs, raws 設為 delete=true

--- webdata/models/News.php
+++ webdata/models/News.php
@@ -12,6 +12,10 @@ class NewsRow extends Pix_Table_Row
         foreach (NewsRaw::search(array('news_id' => $this->id))->order('time ASC') as $raw) {
             $ret = $raw->getInfo();
 
+            if ($ret->title == '0' and $ret->body == '0') {
+                continue;
+            }
+
             $diffs = $diff->diff_main($old_title, $ret->title);
             $patches = $diff->patch_make($diffs);
             $text = $diff->patch_toText($patches);
@@ -45,6 +49,7 @@ class NewsRow extends Pix_Table_Row
             $old_body = $ret->body;
             $old_title = $ret->title;
         }
+        $this->update(arraY('diff_count' => count($this->diffs)));
     }
 }
 
@@ -61,9 +66,10 @@ class News extends Pix_Table
         $this->_columns['url_crc32'] = array('type' => 'int');
         $this->_columns['created_at'] = array('type' => 'int');
         $this->_columns['last_fetch_at'] = array('type' => 'int');
+        $this->_columns['diff_count'] = array('type' => 'int');
 
-        $this->_relations['raws'] = array('rel' => 'has_many', 'type' => 'NewsRaw', 'foreign_key' => 'news_id');
-        $this->_relations['diffs'] = array('rel' => 'has_many', 'type' => 'NewsDiff', 'foreign_key' => 'news_id');
+        $this->_relations['raws'] = array('rel' => 'has_many', 'type' => 'NewsRaw', 'foreign_key' => 'news_id', 'delete' => true);
+        $this->_relations['diffs'] = array('rel' => 'has_many', 'type' => 'NewsDiff', 'foreign_key' => 'news_id', 'delete' => true);
 
         $this->addIndex('url_crc32', array('url_crc32'), 'unique');
     }
