4cb8ee36e0737a49ffaebf4e248349b6a6eb60ff
Ronny Wang <ronnywang@gmail.com>
2013-08-13 11:10:50 +0800

    add KeyValue model

---
+++ webdata/models/KeyValue.php
@@ -0,0 +1,25 @@
+<?php
+
+class KeyValue extends Pix_Table
+{
+    public function init()
+    {
+        $this->_name = 'key_value';
+        $this->_primary = 'key';
+
+        $this->_columns['key'] = array('type' => 'char', 'size' => 32);
+        $this->_columns['value'] = array('type' => 'text');
+    }
+
+    public static function set($key, $value)
+    {
+        try {
+            KeyValue::insert(array(
+                'key' => $key,
+                'value' => $value,
+            ));
+        } catch (Pix_Table_DuplicateException $e) {
+            KeyValue::search(array('key' => $key))->update(array('value' => $value));
+        }
+    }
+}
