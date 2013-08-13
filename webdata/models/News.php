c9f3c2103efaf4151942f556f060dcaf518b775a
Ronny Wang <ronnywang@gmail.com>
2013-08-13 21:04:25 +0800

    增加 News->generateDiff() , 產生 diff

--- webdata/models/News.php
+++ webdata/models/News.php
@@ -1,11 +1,61 @@
 <?php
 
+class NewsRow extends Pix_Table_Row
+{
+    public function generateDiff()
+    {
+        require_once(__DIR__ . '/../stdlibs/diff_match_patch-php-master/diff_match_patch.php');
+        $diff = new diff_match_patch;
+        mb_internal_encoding('UTF-8');
+
+        $old_title = '';
+        $old_body = '';
+        foreach (NewsRaw::search(array('news_id' => $this->id))->order('time ASC') as $raw) {
+            $ret = $raw->getInfo();
+
+            $diffs = $diff->diff_main($old_title, $ret->title);
+            $patches = $diff->patch_make($diffs);
+            $text = $diff->patch_toText($patches);
+            if ($text) {
+                try {
+                    NewsDiff::insert(array(
+                        'news_id' => $this->id,
+                        'time' => $raw->time,
+                        'column' => 0,
+                        'diff' => $text,
+                    ));
+                } catch (Pix_Table_DuplicateException $e) {
+                }
+            }
+
+            $diffs = $diff->diff_main($old_body, $ret->body);
+            $patches = $diff->patch_make($diffs);
+            $text = $diff->patch_toText($patches);
+            if ($text) {
+                try {
+                    NewsDiff::insert(array(
+                        'news_id' => $this->id,
+                        'time' => $raw->time,
+                        'column' => 1,
+                        'diff' => $text,
+                    ));
+                } catch (Pix_Table_DuplicateException $e) {
+                }
+            }
+
+            $old_body = $ret->body;
+            $old_title = $ret->title;
+        }
+    }
+}
+
 class News extends Pix_Table
 {
     public function init()
     {
         $this->_name = 'news';
         $this->_primary = 'id';
+        $this->_rowClass = 'NewsRow';
 
         $this->_columns['id'] = array('type' => 'int', 'auto_increment' => true);
         $this->_columns['url'] = array('type' => 'varchar', 'size' => 255);
