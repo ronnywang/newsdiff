de92160e6aae04b3322dc572f61ba862e4db5022
Ronny Wang <ronnywang@gmail.com>
2013-08-13 10:16:27 +0800

    0.5 秒只抓一個網頁，避免太快被擋

--- webdata/models/Crawler.php
+++ webdata/models/Crawler.php
@@ -2,8 +2,16 @@
 
 class Crawler
 {
+    protected static $_last_fetch = null;
+
     public static function getBody($url)
     {
+        // 0.5 秒只抓一個網頁，以免太快被擋
+        while (!is_null(self::$_last_fetch) and (microtime(true) - self::$_last_fetch) < 0.5) {
+            usleep(1000);
+        }
+
+        self::$_last_fetch = microtime(true);
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $content = curl_exec($curl);
