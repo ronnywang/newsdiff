4910af997bb6048bb8e6b83b7e8a2cb46d64a6a2
Ronny Wang <ronnywang@gmail.com>
2013-08-13 18:16:38 +0800

    加上中央社的 parser

--- webdata/models/Crawler/CNA.php
+++ webdata/models/Crawler/CNA.php
@@ -23,4 +23,32 @@ class Crawler_CNA
         }
 
     }
+
+    public static function parse($body)
+    {
+        if (preg_match('/<title>404<\/title>/', $body)) {
+            $ret = new StdClass;
+            $ret->title = '404';
+            $ret->body = '404';
+            return $ret;
+        }
+        $doc = new DOMDocument('1.0', 'UTF-8');
+        @$doc->loadHTML($body);
+        $ret = new StdClass;
+
+        foreach ($doc->getElementsByTagName('div') as $div_dom) {
+            if ($div_dom->getAttribute('class') == 'news_content') {
+                $ret->title = $div_dom->getElementsByTagName('h1')->item(0)->nodeValue;
+                foreach ($div_dom->getElementsByTagName('div') as $child_div_dom) {
+                    if ($child_div_dom->getAttribute('class') == 'box_2') {
+                        $ret->body = trim(Crawler::getTextFromDom($child_div_dom));
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+
+        return $ret;
+    }
 }
