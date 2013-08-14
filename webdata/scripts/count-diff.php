59c64a7a4f02f1fe36fcf9572a8ddd531134f5cd
Ronny Wang <ronnywang@gmail.com>
2013-08-14 10:42:32 +0800

    增加計算 diff 的 script

---
+++ webdata/scripts/count-diff.php
@@ -0,0 +1,12 @@
+<?php
+
+include(__DIR__ . '/../init.inc.php');
+Pix_Table::$_save_memory = true;
+
+$fetch_at = intval(KeyValue::get('countdiff-time'));
+foreach (News::search("last_fetch_at > $fetch_at")->order('last_fetch_at ASC') as $news) {
+    $news->generateDiff();
+}
+if ($news) {
+    KeyValue::set('countdiff-time', $news->last_fetch_at);
+}
