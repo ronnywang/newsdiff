a39dfeb5d524e8e346819f667b55f9ade7f34c50
Ronny Wang <ronnywang@gmail.com>
2013-08-14 19:14:42 +0800

    每一次都更新 countdiff-time, 這樣子失敗的話就可以從失敗的地方開始

--- webdata/scripts/count-diff.php
+++ webdata/scripts/count-diff.php
@@ -6,7 +6,5 @@ Pix_Table::$_save_memory = true;
 $fetch_at = intval(KeyValue::get('countdiff-time'));
 foreach (News::search("last_fetch_at >= $fetch_at")->order('last_fetch_at ASC') as $news) {
     $news->generateDiff();
-}
-if ($news) {
     KeyValue::set('countdiff-time', $news->last_fetch_at);
 }
