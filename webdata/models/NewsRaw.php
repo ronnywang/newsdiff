f347721a90cc82f91a86723972927708eb321d9e
Ronny Wang <ronnywang@gmail.com>
2013-08-14 10:47:17 +0800

    拿掉 converted_at column

--- webdata/models/NewsRaw.php
+++ webdata/models/NewsRaw.php
@@ -70,6 +70,5 @@ class NewsRaw extends Pix_Table
         $this->_columns['news_id'] = array('type' => 'int');
         $this->_columns['time'] = array('type' => 'int');
         $this->_columns['raw'] = array('type' => 'text');
-        $this->_columns['converted_at'] = array('type' => 'int');
     }
 }
