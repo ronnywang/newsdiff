038885db6c4aacaa44e51c8c64116c03df2a6a8f
Ronny Wang <ronnywang@gmail.com>
2013-08-15 10:13:45 +0800

    加上聯合新聞網的 parser

--- webdata/models/Crawler/UDN.php
+++ webdata/models/Crawler/UDN.php
@@ -15,4 +15,29 @@ class Crawler_UDN
             }
         }
     }
+
+    public static function parse($body)
+    {
+        $body = str_replace('<meta content="text/html; charset=big5" http-equiv="Content-Type">', '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">', $body);
+        $body = str_replace('<meta http-equiv="Content-Type" content="text/html; charset=big5">', '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">', $body);
+
+        $doc = new DOMDocument('1.0', 'UTF-8');
+        @$doc->loadHTML($body);
+        $ret = new StdClass;
+        $ret->title = trim($doc->getElementById('story_title')->nodeValue);
+        if ($doc->getElementById('story')) {
+            $ret->body = trim($doc->getElementById('story_author')->nodeValue . "\n"
+                . $doc->getElementById('story_update')->nodeValue . "\n"
+                . Crawler::getTextFromDom($doc->getElementById('story'))
+            );
+        }
+
+        if (!$ret->title and !$ret->body) {
+            if (preg_match('#<script language=javascript>window.location.href="([^"]*)";</script>#', $body, $matches)) {
+                $ret->title = trim($doc->getElementsByTagName('title')->item(0)->nodeValue);
+                $ret->body = '重新導向至: ' . $matches[1];
+            }
+        }
+        return $ret;
+    }
 }
