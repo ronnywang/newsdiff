eeb38fe045eabc6f8d602435756dbfe296717a4b
Ronny Wang <ronnywang@gmail.com>
2013-08-16 22:57:57 +0800

    十分鐘如果沒有做過就重作，避免 crawler-one 死掉就沒再跑了

--- webdata/scripts/crawler-one.php
+++ webdata/scripts/crawler-one.php
@@ -2,7 +2,8 @@
 
 include(__DIR__ . '/../init.inc.php');
 
-if (KeyValue::get('crawlering')) {
+// 10 分鐘內如果沒有 crawler-one 就重作
+if ($time = intval(KeyValue::get('crawlering')) and time() - $time < 600) {
     exit;
 }
 KeyValue::set('crawlering', time());
