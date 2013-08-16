9ce17f849db2db2a52d79f0ff9ac16f364c25200
Ronny Wang <ronnywang@gmail.com>
2013-08-16 15:25:47 +0800

    加上搜尋新聞功能

--- webdata/controllers/IndexController.php
+++ webdata/controllers/IndexController.php
@@ -28,4 +28,20 @@ class IndexController extends Pix_Controller
         $this->view->news_array = News::search(array('source' => intval($source_id)))->order('last_changed_at DESC')->limit(30);
         return $this->redraw('/index/index.phtml');
     }
+
+    public function searchAction()
+    {
+        if ($news = News::find_by_url_crc32(crc32($_GET['q']))) {
+            return $this->redirect('/index/log/' . $news->id);
+        }
+
+        $terms = explode('/', $_GET['q']);
+        $last_term = array_pop($terms);
+        array_push($terms, urlencode($last_term));
+        if ($news = News::find_by_url_crc32(crc32(implode('/', $terms)))) {
+            return $this->redirect('/index/log/' . $news->id);
+        }
+
+        return $this->alert('not found', '/');
+    }
 }
