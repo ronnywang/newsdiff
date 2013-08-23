4bf9ed2b4c6c910f80c0273b6195c850b14d5cc8
Ronny Wang <ronnywang@gmail.com>
2013-08-23 11:57:08 +0800

    加上新的 count-diff

--- webdata/scripts/count-diff.php
+++ webdata/scripts/count-diff.php
@@ -2,9 +2,15 @@
 
 include(__DIR__ . '/../init.inc.php');
 Pix_Table::$_save_memory = true;
+Pix_Table::addStaticResultSetHelper('Pix_Array_Volume');
 
+$start_at = time();
 $fetch_at = intval(KeyValue::get('countdiff-time'));
-foreach (News::search("last_fetch_at >= $fetch_at")->order('last_fetch_at ASC') as $news) {
+foreach (News::search("last_fetch_at > last_diff_at")->order('last_fetch_at DESC')->volumemode(1000) as $news) {
     $news->generateDiff();
     KeyValue::set('countdiff-time', $news->last_fetch_at);
+    // 最多只跑五分鐘
+    if (time() - $start_at > 300) {
+        exit;
+    }
 }
