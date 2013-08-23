4d5ad9694d7ae1ba7775acfc1380a8ec2e6a19a1
Ronny Wang <ronnywang@gmail.com>
2013-08-23 22:33:05 +0800

    count-diff 的 generateDiff 加上 false 讓他不用完全重作

--- webdata/scripts/count-diff.php
+++ webdata/scripts/count-diff.php
@@ -4,13 +4,6 @@ include(__DIR__ . '/../init.inc.php');
 Pix_Table::$_save_memory = true;
 Pix_Table::addStaticResultSetHelper('Pix_Array_Volume');
 
-$start_at = time();
-$fetch_at = intval(KeyValue::get('countdiff-time'));
-foreach (News::search("last_fetch_at > last_diff_at")->order('last_fetch_at DESC')->volumemode(1000) as $news) {
-    $news->generateDiff();
-    KeyValue::set('countdiff-time', $news->last_fetch_at);
-    // 最多只跑五分鐘
-    if (time() - $start_at > 300) {
-        exit;
-    }
+foreach (News::search("last_fetch_at > last_diff_at + 1")->order('last_fetch_at DESC')->volumemode(1000) as $news) {
+    $news->generateDiff(false);
 }
