62e8b47b09e51218e22046531ffd625275201bd6
Ronny Wang <ronnywang@gmail.com>
2013-08-26 11:31:05 +0800

    改成 order id 就會 unique 不會有跳過

--- webdata/scripts/count-diff.php
+++ webdata/scripts/count-diff.php
@@ -4,6 +4,6 @@ include(__DIR__ . '/../init.inc.php');
 Pix_Table::$_save_memory = true;
 Pix_Table::addStaticResultSetHelper('Pix_Array_Volume');
 
-foreach (News::search("last_fetch_at > last_diff_at + 1")->order('last_fetch_at DESC')->volumemode(1000) as $news) {
+foreach (News::search("last_fetch_at > last_diff_at + 1")->order('id')->volumemode(1000) as $news) {
     $news->generateDiff(false);
 }
