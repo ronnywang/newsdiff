7b6e316431a88abf681eaca9033a66f3c13824ff
Ronny Wang <ronnywang@gmail.com>
2013-08-30 14:20:09 +0800

    把 getInfo 搬出去成 static function

--- webdata/models/NewsRaw.php
+++ webdata/models/NewsRaw.php
@@ -6,61 +6,8 @@ class NewRawRow extends Pix_Table_Row
     {
         $news = News::find($this->news_id);
         $url = $news->url;
-        $host = parse_url($url, PHP_URL_HOST);
 
-        if (strlen($this->raw) < 10) {
-            $ret = new StdClass;
-            $ret->title = $ret->body = $this->raw;
-            return $ret;
-        }
-
-        switch ($host) {
-        case 'www.chinatimes.com':
-            $ret = Crawler_Chinatimes::parse($this->raw);
-            break;
-
-        case 'www.appledaily.com.tw':
-            $ret = Crawler_Appledaily::parse($this->raw);
-            break;
-
-        case 'www.nownews.com':
-            $ret = Crawler_Nownews::parse($this->raw);
-            break;
-
-        case 'www.ettoday.net':
-            $ret = Crawler_Ettoday::parse($this->raw);
-            break;
-
-        case 'newtalk.tw':
-            $ret = Crawler_Newtalk::parse($this->raw);
-            break;
-
-        case 'iservice.libertytimes.com.tw':
-            $ret = Crawler_Libertytimes::parse($this->raw);
-            break;
-
-        case 'www.libertytimes.com.tw':
-            $ret = Crawler_Libertytimes::parse2($this->raw);
-            break;
-
-        case 'www.cna.com.tw':
-            $ret = Crawler_CNA::parse($this->raw);
-            break;
-
-        case 'udn.com':
-            $ret = Crawler_UDN::parse($this->raw);
-            break;
-
-        default:
-            throw new Exception('unknown host: ' . $url . ' ' . $this->time);
-        }
-
-        if (!$ret->title or !$ret->body) {
-            $ret->title = $ret->body = '無法判斷的內容';
-            error_log('找不到內容:' . $url . ' ' . $this->time);
-        }
-
-        return $ret;
+        return NewsRaw::getInfo($this->raw, $url);
     }
 }
 
@@ -75,5 +22,64 @@ class NewsRaw extends Pix_Table
         $this->_columns['news_id'] = array('type' => 'int');
         $this->_columns['time'] = array('type' => 'int');
         $this->_columns['raw'] = array('type' => 'text');
+    }
+
+    public static function getInfo($raw, $url)
+    {
+        $host = parse_url($url, PHP_URL_HOST);
+
+        if (strlen($raw) < 10) {
+            $ret = new StdClass;
+            $ret->title = $ret->body = $raw;
+            return $ret;
+        }
+
+        switch ($host) {
+        case 'www.chinatimes.com':
+            $ret = Crawler_Chinatimes::parse($raw);
+            break;
+
+        case 'www.appledaily.com.tw':
+            $ret = Crawler_Appledaily::parse($raw);
+            break;
+
+        case 'www.nownews.com':
+            $ret = Crawler_Nownews::parse($raw);
+            break;
+
+        case 'www.ettoday.net':
+            $ret = Crawler_Ettoday::parse($raw);
+            break;
+
+        case 'newtalk.tw':
+            $ret = Crawler_Newtalk::parse($raw);
+            break;
+
+        case 'iservice.libertytimes.com.tw':
+            $ret = Crawler_Libertytimes::parse($raw);
+            break;
+
+        case 'www.libertytimes.com.tw':
+            $ret = Crawler_Libertytimes::parse2($raw);
+            break;
+
+        case 'www.cna.com.tw':
+            $ret = Crawler_CNA::parse($raw);
+            break;
+
+        case 'udn.com':
+            $ret = Crawler_UDN::parse($raw);
+            break;
+
+        default:
+            throw new Exception('unknown host: ' . $url);
+        }
+
+        if (!$ret->title or !$ret->body) {
+            $ret->title = $ret->body = '無法判斷的內容';
+            error_log('找不到內容:' . $url);
+        }
+
+        return $ret;
     }
 }
