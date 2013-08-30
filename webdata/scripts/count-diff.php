d0cf1396324c0cee1005ddb9eb6f5468f2c3582f
Ronny Wang <ronnywang@gmail.com>
2013-08-30 17:26:11 +0800

    刪除 count-diff.php, 已經併在 fetch-one 下面了

--- webdata/scripts/count-diff.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include(__DIR__ . '/../init.inc.php');
-Pix_Table::$_save_memory = true;
-Pix_Table::addStaticResultSetHelper('Pix_Array_Volume');
-
-foreach (News::search("last_fetch_at > last_diff_at + 1")->order('id')->volumemode(1000) as $news) {
-    $news->generateDiff(false);
-}
