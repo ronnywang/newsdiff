2f3ef883983862cbf515b508d68288c4876c3427
Ronny Wang <ronnywang@gmail.com>
2013-11-15 11:43:20 +0800

    add api

---
+++ webdata/controllers/ApiController.php
@@ -0,0 +1,23 @@
+<?php
+
+class ApiController extends Pix_Controller
+{
+    public function newsAction()
+    {
+        $url = $_GET['url'];
+
+        if (!$news = News::find_by_url_crc32(crc32($url))) {
+            return $this->json(array('error' => true, 'message' => '找不到這則新聞'));
+        }
+
+        $infos = array();
+        foreach ($news->infos->order('time ASC') as $news_info) {
+            $info = new StdClass;
+            $info->time = intval($news_info->time);
+            $info->title = strval($news_info->title);
+            $info->body = strval($news_info->body);
+            $infos[] = $info;
+        }
+        return $this->json($infos);
+    }
+}
