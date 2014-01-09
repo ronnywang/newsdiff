a507613e3d85cdaf620058c47508ee3e788a8e0a
Ronny Wang <ronnywang@gmail.com>
2014-01-09 13:34:38 +0800

    改寫成用 curl_multi

--- webdata/models/Crawler.php
+++ webdata/models/Crawler.php
@@ -2,65 +2,27 @@
 
 class Crawler
 {
-    protected static $_last_fetch = null;
-
     public static function standardURL($url)
     {
         $url = preg_replace_callback('/[^\x00-\xff]*/u', function($m) { return urlencode($m[0]); }, $url);
         return $url;
     }
 
-    public static function getBody($url, $wait = 0.5, $throw_exception = true, $retry = 3)
-    {
-        $url = self::standardURL($url);
-        // 0.5 秒只抓一個網頁，以免太快被擋
-        while (!is_null(self::$_last_fetch) and (microtime(true) - self::$_last_fetch) < $wait) {
-            usleep(1000);
-        }
-
-        self::$_last_fetch = microtime(true);
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-        curl_setopt($curl, CURLOPT_TIMEOUT, 20);
-        $content = curl_exec($curl);
-        $info = curl_getinfo($curl);
-        curl_close($curl);
-
-        if (200 !== $info['http_code']) {
-            if ($retry > 0) {
-                // 重試三次
-                return self::getBody($url, $wait, $throw_exception, $retry - 1);
-            }
-
-            if ($throw_exception) {
-                throw new Exception('not 200', $info['http_code']);
-            } else {
-                error_log('not 200: ' . $url);
-                return '';
-            }
-        }
-        return $content;
-    }
-
-    public static function fetchRaw($news, $wait_time = 0)
+    public static function updateContent($news, $content)
     {
         $now = time();
 
-        KeyValue::set('crawling', $news->url);
-        try {
-            $content = self::getBody($news->url, $wait_time);
-        } catch (Exception $e) {
-            $content = $e->getCode();
-        }
         if (preg_match('/content="text\/html; charset=big5/', $content)) {
             $content = iconv('big5', 'utf-8', $content);
         }
 
         $last_info = $news->infos->order('`time` DESC')->first();
         $ret = NewsRaw::getInfo($content, $news->url);
-        $news->update(array('last_fetch_at' => $now));
+        $news->update(array(
+            'last_fetch_at' => time(),
+            'error_count' => 0,
+        ));
+
         if (!$last_info or $ret->title != $last_info->title or $ret->body != $last_info->body) {
             NewsRaw::insertNew(array(
                 'news_id' => $news->id,
@@ -87,29 +49,68 @@ class Crawler
     public static function updateAllRaw()
     {
         $now = time();
+        $start = microtime(true);
         $fetching_news = array();
         $count = 0;
         foreach (News::search("created_at > $now - 86400 AND last_fetch_at < $now - 3600") as $news) {
-            $fetching_news[$news->source][] = $news;
+            $fetching_news[] = $news;
             $count ++;
         }
         if ($count) {
             error_log("fetching $count news...");
         }
 
-        $last_source = null;
-        // 每個 source 輪流抓一次, 比較分散，這樣子也可以不用 sleep
-        while (count($fetching_news)) {
-            foreach (array_keys($fetching_news) as $source) {
-                // 跟上一個同來源才要睡 0.5 秒
-                self::fetchRaw(array_pop($fetching_news[$source]), $last_source == $source ? 0.5 : 0);
+        $handles = array();
+        $mh = curl_multi_init();
+        curl_multi_setopt($mh, CURLMOPT_PIPELINING, true);
+        curl_multi_setopt($mh, CURLMOPT_MAXCONNECTS, 10);
 
-                if (count($fetching_news[$source]) == 0) {
-                    unset($fetching_news[$source]);
-                }
-                $last_source = $source;
+        foreach ($fetching_news as $id => $news) {
+            $curl = curl_init(self::standardURL($news->url));
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+            curl_setopt($curl, CURLOPT_TIMEOUT, 20);
+
+            curl_multi_add_handle($mh, $curl);
+            $handles[$id] = $curl;
+        }
+
+        KeyValue::set('crawling', "curl_multi_exec ing : {$count}");
+        do {
+            $mrc = curl_multi_exec($mh, $active);
+        } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+
+        while ($active and $mrc == CURLM_OK) {
+            if (curl_multi_select($mh) != -1) {
+                do {
+                    $mrc = curl_multi_exec($mh, $active);
+                } while ($mrc == CURLM_CALL_MULTI_PERFORM);
             }
         }
+
+        KeyValue::set('crawling', "curl_multi_getcontent : {$count}");
+
+        $status_count = array();
+        foreach ($handles as $index => $curl) {
+            $content = curl_multi_getcontent($curl);
+            $info = curl_getinfo($curl);
+            $news = $fetching_news[$index];
+
+            if ($info['http_code'] != 200) {
+                if ($news->error_count > 3) {
+                    error_log("{$news->url} {$info['http_code']}");
+                    self::updateContent($news, $info['http_code']);
+                    continue;
+                }
+                $news->update(array('error_count' => $news->error_count + 1));
+                $status_count[$news->source . '-' . intval($info['http_code'])] ++;
+                continue;
+            }
+            self::updateContent($news, $content);
+        }
+        $spent = microtime(true) - $start;
+        error_log('finish: ' . json_encode($status_count) . 'spent: ' . $spent);
     }
 
     public function getTextFromDom($node)
