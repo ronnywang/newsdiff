c50072a8eed3fd3075f452977f2601d432453503
Ronny Wang <ronnywang@gmail.com>
2014-04-17 21:39:13 +0800

    加上不重覆檢查

--- webdata/scripts/crawler-part.php
+++ webdata/scripts/crawler-part.php
@@ -4,6 +4,10 @@ include(__DIR__ . '/../init.inc.php');
 
 $part = $_SERVER['argv'][1];
 $total = $_SERVER['argv'][2];
+
+if ($_SERVER['argv'][1] != 'force' and $time = intval(KeyValue::get('crawlering-' . $part)) and time() - $time < 600) {
+    exit;
+}
 KeyValue::set('crawlering-' . $part, time());
 
 Pix_Table::$_save_memory = true;
