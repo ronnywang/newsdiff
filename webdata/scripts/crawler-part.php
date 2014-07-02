63a480e33e73c0430b2f9bbf4d2121a6f7dfce26
Ronny Wang <ronnywang@gmail.com>
2014-07-02 21:29:08 +0800

    改成 $part, $total 可以分開 cache

--- webdata/scripts/crawler-part.php
+++ webdata/scripts/crawler-part.php
@@ -5,14 +5,15 @@ include(__DIR__ . '/../init.inc.php');
 $part = $_SERVER['argv'][1];
 $total = $_SERVER['argv'][2];
 
-if ($_SERVER['argv'][1] != 'force' and $time = intval(KeyValue::get('crawlering-' . $part)) and time() - $time < 600) {
+$key = "crawlering-{$part}-{$total}";
+if ($_SERVER['argv'][3] != 'force' and $time = intval(KeyValue::get($key)) and time() - $time < 600) {
     exit;
 }
-KeyValue::set('crawlering-' . $part, time());
+KeyValue::set($key, time());
 
 Pix_Table::$_save_memory = true;
 Crawler::updatePart($part, $total);
 
-KeyValue::set('crawlering-' . $part, 0);
+KeyValue::set($key, 0);
 // 清掉網址
 KeyValue::set('crawling-' . $part, '');
