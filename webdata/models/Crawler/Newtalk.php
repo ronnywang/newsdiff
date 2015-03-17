91ace8104823452e01ca106e6dbfe0ab8a027bf2
Ronny Wang <ronnywang@gmail.com>
2015-03-17 11:40:07 +0800

    更新新頭殼 parser

--- webdata/models/Crawler/Newtalk.php
+++ webdata/models/Crawler/Newtalk.php
@@ -5,12 +5,12 @@ class Crawler_Newtalk
     public static function crawl($insert_limit)
     {
         $content = Crawler::getBody('http://newtalk.tw');
-        $content .= Crawler::getBody('http://newtalk.tw/rss_news.php');
-        for ($i = 1; $i >= 14; $i ++) {
-            $content .= Crawler::getBody('http://newtalk.tw/rss_news.php?oid=' . $i);
+        $content .= Crawler::getBody('http://newtalk.tw/rss/all');
+        for ($i = 1; $i <= 5; $i ++) {
+            $content .= Crawler::getBody('http://newtalk.tw/rss/category/' . $i);
         }
 
-        preg_match_all('#http://newtalk.tw\/news/\d+/\d+/\d+/\d+\.html#', $content, $matches);
+        preg_match_all('#http://newtalk.tw\/news/view/\d+-\d+-\d+/\d+#', $content, $matches);
         $insert = $update = 0;
         foreach ($matches[0] as $link) {
             $update ++;
@@ -29,13 +29,14 @@ class Crawler_Newtalk
         @$doc->loadHTML($body);
         $ret = new StdClass;
         foreach ($doc->getElementsByTagName('div') as $div_dom) {
-            if ($div_dom->getAttribute('class') == 'cont_main_tit news_cont_area_tit') {
-                $ret->title = $div_dom->getElementsByTagName('label')->item(0)->nodeValue;
+            if ($div_dom->getAttribute('class') == 'content_title') {
+                $ret->title = $div_dom->nodeValue;
             }
-            if ($div_dom->getAttribute('class') == 'news_ctxt_area_word') {
-                $ret->body = trim(Crawler::getTextFromDom($div_dom));
+            if ($div_dom->getAttribute('class') == 'content_reporter') {
+                $reporter = Crawler::getTextFromDom($div_dom);
             }
         }
+        $ret->body = $reporter . "\n" . trim($doc->getElementById('news_content')->nodeValue);
 
         return $ret;
     }
