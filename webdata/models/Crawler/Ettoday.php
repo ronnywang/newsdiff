454bf01481c24942c99b5ee55e0732170c03266a
Ronny Wang <ronnywang@gmail.com>
2015-10-08 15:22:07 +0800

    加上 ettoday 運動新聞的版型

--- webdata/models/Crawler/Ettoday.php
+++ webdata/models/Crawler/Ettoday.php
@@ -26,23 +26,52 @@ class Crawler_Ettoday
         $doc = new DOMDocument('1.0', 'UTF-8');
         @$doc->loadHTML($body);
         $ret = new StdClass;
+        $type = null;
         foreach ($doc->getElementsByTagName('h2') as $h2_dom) {
             if ($h2_dom->getAttribute('itemprop') == 'headline') {
                 $ret->title = trim($h2_dom->nodeValue);
+                $type = 1;
+                break;
+            }
+
+            if ($h2_dom->getAttribute('class') == 'title') {
+                $ret->title = trim($h2_dom->nodeValue);
+                $type = 2;
                 break;
             }
         }
-        foreach ($doc->getElementsByTagName('sectione') as $sectione_dom) {
-            if ($sectione_dom->getAttribute('itemprop') == 'articleBody') {
-                $ret->body = '';
-                foreach ($sectione_dom->childNodes as $node) {
-                    if ($node->nodeType == XML_ELEMENT_NODE and $node->getAttribute('class') == 'test-keyword') {
-                        break;
-                    }
-                    $ret->body .= Crawler::getTextFromDom($node);
+
+        if (is_null($type)) {
+            foreach ($doc->getElementsByTagName('h1') as $h1_dom) {
+                if ($h1_dom->getAttribute('class') == 'title') {
+                    $ret->title = trim($h1_dom->nodeValue);
+                    $type = 2;
+                    break;
                 }
-                break;
             }
+        }
+
+        if ($type == 1) {
+            foreach ($doc->getElementsByTagName('sectione') as $sectione_dom) {
+                if ($sectione_dom->getAttribute('itemprop') == 'articleBody') {
+                    $ret->body = '';
+                    foreach ($sectione_dom->childNodes as $node) {
+                        if ($node->nodeType == XML_ELEMENT_NODE and $node->getAttribute('class') == 'test-keyword') {
+                            break;
+                        }
+                        $ret->body .= Crawler::getTextFromDom($node);
+                    }
+                    break;
+                }
+            }
+        } elseif ($type == 2) {
+            foreach ($doc->getElementsByTagName('div') as $div_dom) {
+                if ($div_dom->getAttribute('class') == 'story') {
+                    $ret->body = Crawler::getTextFromDom($div_dom);
+                }
+            }
+        } else {
+            throw new Exception('無法正常解析');
         }
 
         return $ret;
