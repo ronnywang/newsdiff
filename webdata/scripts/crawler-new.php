81cf3888f6d788bd8ec5362a6f4687a7b5746e04
Ronny Wang <ronnywang@gmail.com>
2015-11-30 23:58:00 +0800

    打亂新聞來源，並加上跑 8 分鐘限制，避免都集中在前面來源

--- webdata/scripts/crawler-new.php
+++ webdata/scripts/crawler-new.php
@@ -22,12 +22,24 @@ $crawlers = array(
     16 => 'Crawler_StormMediaGroup'
 );
 
+$start = microtime(true);
 $insert_count = 0;
 $max_insert = 500;
-foreach ($crawlers as $id => $class) {
+$keys = array_keys($crawlers);
+// 隨機跑新聞來源，避免在前面的總是優先被跑到
+shuffle($keys);
+
+foreach ($keys as $id) {
+    $class = $crawlers[$id];
     if ($max_insert - $insert_count <= 0) {
         break;
     }
+    // 超過 8 分鐘就不要跑了...以免累積太多 job
+    if (microtime(true) - $start > 8 * 60) {
+        error_log("excess 8 minutes");
+        break;
+    }
+
     try {
         list($update, $insert) = call_user_func(array($class, 'crawl'), $max_insert - $insert_count);
     } catch (Exception $e) {
