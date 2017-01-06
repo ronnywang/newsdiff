0a81f2637458f4e5a892bb2653986d7f34190929
Ronny Wang <ronnywang@gmail.com>
2017-01-06 15:37:17 +0800

    fix ttv crawler by kiang

--- webdata/models/Crawler/TTV.php
+++ webdata/models/Crawler/TTV.php
@@ -1,16 +1,19 @@
 <?php
 
-class Crawler_TTV
-{
-    public static function crawl($insert_limit)
-    {
-        $content = Crawler::getBody('http://www.ttv.com.tw/news/');
-        preg_match_all('#/[0-9]+/[0-9]+/[0-9]+/[0-9]+[0-9A-Z]\.htm#', $content, $matches);
-        $links = array_unique($matches[0]);
+class Crawler_TTV {
+
+    public static function crawl($insert_limit) {
+        $cats = array('A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'P');
+        $content = '';
+        foreach ($cats AS $cat) {
+            $content .= Crawler::getBody('http://www.ttv.com.tw/news/catlist/' . $cat);
+        }
+        preg_match_all('#/news/view/default.asp\?i=([0-9a-z]*)([^"]*)#i', $content, $matches);
+        $links = array_unique($matches[1]);
         $insert = $update = 0;
         foreach ($links as $link) {
             $update ++;
-            $link = 'http://www.ttv.com.tw' . $link;
+            $link = 'http://www.ttv.com.tw/news/view/' . $link;
             $insert += News::addNews($link, 12);
             if ($insert_limit <= $insert) {
                 break;
@@ -19,33 +22,25 @@ class Crawler_TTV
         return array($update, $insert);
     }
 
-    public static function parse($body)
-    {
+    public static function parse($body) {
         $ret = new StdClass;
 
         $doc = new DOMDocument('1.0', 'UTF-8');
-        $body = str_replace('text/html; charset=big5', 'text/html; charset=utf-8', $body);
+        $body = str_replace('<meta charset="utf-8">', '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">', $body);
 
-        if (FALSE !== strpos($body, '<title>錯誤代碼：404</title>')) {
-            $ret->title = $ret->body = 404;
-            return $ret;
-        }
         @$doc->loadHTML($body);
-        if (!$click_safeguard_dom = $doc->getElementById('iCliCK_SafeGuard')) {
-            return null;
-        }
-        if (!$clickBody_start_dom = $doc->getElementById('iclickAdBody_Start')) {
-            return null;
-        }
-        $ret->title = str_replace("\n", "", trim($click_safeguard_dom->nodeValue));
-        $ret->body = '';
-        $dom = $clickBody_start_dom;
-        while ($dom = $dom->nextSibling) {
-            if ($dom->nodeType == XML_ELEMENT_NODE and $dom->getAttribute('id') == 'iclickAdBody_End') {
-                break;
+
+        $ret->title = trim($doc->getElementsByTagName('h1')->item(0)->nodeValue);
+
+        $blocks = array();
+        $doms = $doc->getElementsByTagName('div');
+        foreach ($doms AS $dom) {
+            if ($dom->getAttribute('class') === 'content') {
+                $blocks[] = trim(Crawler::getTextFromDom($dom));
             }
-            $ret->body .= Crawler::getTextFromDom($dom);
         }
+        $ret->body = implode("\n", $blocks);
         return $ret;
     }
+
 }
