7e262c69f0d262f5668f9efdb2c375753ee783e4
Ronny Wang <ronnywang@gmail.com>
2017-01-07 16:52:05 +0800

    加回三立

--- webdata/scripts/crawler-new.php
+++ webdata/scripts/crawler-new.php
@@ -18,7 +18,7 @@ $crawlers = array(
     12 => 'Crawler_TTV',
     13 => 'Crawler_CTS',
     14 => 'Crawler_FTV',
-//    15 => 'Crawler_SETNews',
+    15 => 'Crawler_SETNews',
     16 => 'Crawler_StormMediaGroup'
 );
 
