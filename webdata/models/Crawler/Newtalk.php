240dbdfde4efd0d21c347d14d603189158471a4d
Ronny Wang <ronnywang@gmail.com>
2017-04-15 03:06:56 +0800

    新頭殼換 https

--- webdata/models/Crawler/Newtalk.php
+++ webdata/models/Crawler/Newtalk.php
@@ -4,13 +4,13 @@ class Crawler_Newtalk
 {
     public static function crawl($insert_limit)
     {
-        $content = Crawler::getBody('http://newtalk.tw');
-        $content .= Crawler::getBody('http://newtalk.tw/rss/all');
+        $content = Crawler::getBody('https://newtalk.tw');
+        $content .= Crawler::getBody('https://newtalk.tw/rss/all');
         for ($i = 1; $i <= 5; $i ++) {
-            $content .= Crawler::getBody('http://newtalk.tw/rss/category/' . $i);
+            $content .= Crawler::getBody('https://newtalk.tw/rss/category/' . $i);
         }
 
-        preg_match_all('#https?://newtalk.tw\/news/view/\d+-\d+-\d+/\d+#', $content, $matches);
+        preg_match_all('#https://newtalk.tw\/news/view/\d+-\d+-\d+/\d+#', $content, $matches);
         $insert = $update = 0;
         foreach ($matches[0] as $link) {
             $update ++;
