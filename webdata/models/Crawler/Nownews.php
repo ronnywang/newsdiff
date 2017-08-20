ec47ec260981dcb7dae3bba5a6849a3dba225121
Ronny Wang <ronnywang@gmail.com>
2017-08-20 23:38:13 +0800

    更新 nownews 的 parser

--- webdata/models/Crawler/Nownews.php
+++ webdata/models/Crawler/Nownews.php
@@ -22,95 +22,52 @@ class Crawler_Nownews
     {
         $body = mb_convert_encoding($body, 'HTML-ENTITIES', "UTF-8");
         $doc = new DOMDocument('1.0', 'UTF-8');
-        // 移除掉關鍵字的部份
-        $body = preg_replace('#<p class="bzkeyword">.*?</p>#s', '', $body);
         @$doc->loadHTML($body);
-        $ret = new StdClass;
-        foreach ($doc->getElementsByTagName('h1') as $h1_dom) {
-            if ($h1_dom->getAttribute('itemprop') == 'headline') {
-                $ret->title = trim($h1_dom->nodeValue);
+
+        $ret = new STdClass;
+        if (!$h1_dom = $doc->getElementsByTagName('h1')->item(0)) {
+            $ret->title = $ret->body = '無法判斷的內容';
+            return $ret;
+        }
+
+        $ret->title = trim($h1_dom->nodeValue);
+        $ret->body = '';
+
+        // 作者資訊
+        $found = false;
+        foreach ($doc->getElementsByTagName('span') as $span_dom) {
+            if (strpos($span_dom->getAttribute('class'), 'author_') === 0) {
+                $ret->body = trim($ret->body) . "\n" . trim($span_dom->nodeValue);
+                $found = true;
                 break;
             }
         }
-        foreach ($doc->getElementsByTagName('div') as $div_dom) {
-            if ($div_dom->getAttribute('itemprop') == 'articleBody') {
-                $ret->body = '';
-                foreach ($div_dom->childNodes as $childNode) {
-                    if ($childNode->nodeType == XML_ELEMENT_NODE and $childNode->nodeName == 'p' and $childNode->getAttribute('class') == 'bzkeyword') {
-                        break;
-                    }
-                    $ret->body .= Crawler::getTextFromDom($childNode);
-                }
-                $ret->body = trim($ret->body);
+        if (!$found) {
+            $ret->title = $ret->body = '無法判斷的內容';
+            return $ret;
+        }
+
+        // 時間
+        $found = false;
+        foreach ($doc->getElementsByTagName('img') as $img_dom) {
+            if (strpos($img_dom->getAttribute('class'), 'timeIcon_') === 0) {
+                $ret->body = trim($ret->body) . "\n" . trim($img_dom->nextSibling->nodeValue);
+                $found = true;
                 break;
             }
         }
-
-        if (!$ret->title and !$ret->body) { // 可能是星光大道類型
-            foreach ($doc->getElementsByTagName('div') as $div_dom) {
-                if (in_array($div_dom->getAttribute('class'), array('news_story', 'ws_index_main_story'))) {
-                    $ret->title = $div_dom->getElementsByTagName('h1')->item(0)->nodeValue;
-                }
-
-                if ($div_dom->getAttribute('class') == 'story_content') {
-                    $ret->body .= trim(Crawler::getTextFromDom($div_dom));
-                }
-            }
+        if (!$found) {
+            $ret->title = $ret->body = '無法判斷的內容';
+            return $ret;
         }
 
-        if (!$ret->title and !$ret->body and $div_dom = $doc->getElementById('news_container')) {
-            $ret->title = $div_dom->getElementsByTagName('h1')->item(0)->nodeValue;
-            foreach ($div_dom->getElementsByTagName('div') as $child_div_dom) {
-                if ($child_div_dom->getAttribute('class') == 'news_story') {
-                    $ret->body = Crawler::getTextFromDom($child_div_dom);
-                }
-            }
+        // 內文
+        if ($doc->getElementsByTagName('article')->length != 1) {
+            $ret->title = $ret->body = '無法判斷的內容';
+            return $ret;
         }
+        $ret->body = trim($ret->body) . "\n" . Crawler::getTextFromDom($doc->getElementsByTagName('article')->item(0));
 
-        if ((!$ret->title or !$ret->body) and $div_dom = $doc->getElementById('report_file_story')) {
-            $ret->title = $div_dom->getElementsByTagName('h1')->item(0)->nodeValue;
-            foreach ($div_dom->getElementsByTagName('div') as $child_div_dom) {
-                if ($child_div_dom->getAttribute('class') == 'story_content') {
-                    $ret->body = '';
-                    foreach ($child_div_dom->childNodes as $childNode) {
-                        if ($childNode->nodeType == XML_ELEMENT_NODE and $childNode->getAttribute('class') == 'operate_0') {
-                            continue;
-                        }
-                        $ret->body .= Crawler::getTextFromDom($childNode);
-                    }
-                    $ret->body = trim($ret->body);
-                }
-            }
-        }
-
-        if (!$ret->title) {
-            foreach ($doc->getElementsByTagName('h1') as $h1_dom) {
-                $ret->title = $h1_dom->nodeValue;
-
-                foreach ($doc->getElementsByTagName('div') as $div_dom) {
-                    if ($div_dom->getAttribute('class') == 'body') {
-                        $ret->body = trim(Crawler::getTextFromDom($div_dom));
-                        break 2;
-                    }
-                }
-            }
-        }
-
-        $ret->body = preg_replace_callback('#http://[0-9]-ps.googleusercontent.com/([xh])/([^" \n]*).pagespeed.[a-z]*\.[\-_A-Za-z0-9.]*\n?#', function($m) {
-            if ($m[1] == 'x') {
-                $url = str_replace('www.nownews.com/', '', $m[2]);
-            } else {
-                $url = $m[2];
-            }
-            return 'http://' . $url . "\n";
-
-        }, $ret->body);
-
-        $ret->body = preg_replace_callback('#/[^/]*x[_0-9a-z]*\.jpg#i', function($m){
-            return preg_replace('#/[^/]*x#', '/', $m[0]);
-        }, $ret->body);
-
-        $ret->body = trim($ret->body);
         return $ret;
     }
 }
