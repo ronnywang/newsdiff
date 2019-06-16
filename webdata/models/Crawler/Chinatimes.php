2264cf2b98dc3a25b9e7af80ec54293ab87a54e8
Ronny Wang <ronnywang@gmail.com>
2019-06-16 22:54:45 +0800

    update chinatimes parser

--- webdata/models/Crawler/Chinatimes.php
+++ webdata/models/Crawler/Chinatimes.php
@@ -4,24 +4,19 @@ class Crawler_Chinatimes
 {
     public static function crawl($insert_limit)
     {
-        $content = Crawler::getBody('http://www.chinatimes.com');
-        $content .= Crawler::getBody('http://www.chinatimes.com/newspapers/'); // 日報精選
-        $content .= Crawler::getBody('http://www.chinatimes.com/newspapers/2601'); // 中國時報
-        $content .= Crawler::getBody('http://www.chinatimes.com/newspapers/2602'); // 工商時報
-        $content .= Crawler::getBody('http://www.chinatimes.com/newspapers/2603'); // 旺報
-        $content .= Crawler::getBody('http://www.chinatimes.com/newspapers/260109'); // 時論廣場
-        $content .= Crawler::getBody('http://www.chinatimes.com/newspapers/ctw'); // 時周精選
-        $content .= Crawler::getBody('http://www.chinatimes.com/rss/focus.xml');
+        $content = Crawler::getBody('https://www.chinatimes.com');
         for ($i = 1; $i < 10; $i ++) {
-            $content .= Crawler::getBody('http://www.chinatimes.com/realtimenews/?page=' . $i);
+            $content .= Crawler::getBody('https://www.chinatimes.com/realtimenews/?page=' . $i);
+            $content .= Crawler::getBody('https://www.chinatimes.com/newspapers/2601?page=' . $i);
         }
 
         preg_match_all('#/(newspapers|realtimenews)/([^"\#<]*-)?\d+-\d+["<]?#', $content, $matches);
         $insert = $update = 0;
         foreach (array_unique($matches[0]) as $link) {
             $update ++;
-            $url = Crawler::standardURL('http://www.chinatimes.com' . rtrim($link, '"<'));
+            $url = Crawler::standardURL('https://www.chinatimes.com' . rtrim($link, '"<'));
             $insert += News::addNews($url, 2);
+            error_log($url);
             if ($insert_limit <= $insert) {
                 break;
             }
@@ -39,41 +34,26 @@ class Crawler_Chinatimes
         }
         $doc = new DOMDocument;
         @$doc->loadHTML($body);
-        if (!$pagecontainer_dom= Crawler::getDomByNameAndClass($doc, 'div', 'page_container')) {
-            return null;
-        }
-        $header_dom = $pagecontainer_dom->getElementsByTagName('header')->item(0);
         $ret = new StdClass;
-        $ret->title = trim($header_dom->getElementsByTagName('h1')->item(0)->nodeValue);
+        $ret->title = trim($doc->getElementsByTagName('h1')->item(0)->nodeValue);
 
         $content = '';
-        if ($article_info_dom = Crawler::getDomByNameAndClass($doc, 'div', 'article_info')) {
+        if ($article_info_dom = Crawler::getDomByNameAndClass($doc, 'header', 'article-header')) {
             foreach ($article_info_dom->getElementsByTagName('div') as $div_dom) {
-                if ($div_dom->getAttribute('class') == 'reporter') {
+                if ($div_dom->getAttribute('class') == 'meta-info') {
                     $content .= preg_replace('/[\r\n ]+/', ' ', Crawler::getTextFromDom($div_dom)) . "\n";
                     break;
                 }
             }
         }
-        $article_dom = $pagecontainer_dom->getElementsByTagName('article')->item(1);
 
         $body = '';
-        foreach ($article_dom->childNodes as $child_node) {
-            if ($child_node->nodeName == 'div' and $child_node->getAttribute('id') == 'div-inread-ad') {
-                continue;
-            } elseif ($child_node->nodeName == '#comment') {
-                continue;
+        if ($article_dom = Crawler::getDomByNameAndClass($doc, 'div', 'article-body')) {
+            foreach ($article_dom->childNodes as $node) {
+                $body = trim($body . "\n" . Crawler::getTextFromDom($node)) . "\n";
             }
-            $body .= trim($child_node->nodeValue) . "\n";
         } 
         $content .= trim($body) . "\n";
-
-        if ($dom = Crawler::getDomByNameAndClass($doc, 'div', 'editorthis')) {
-            $content .= trim(Crawler::getTextFromDom($dom)) . "\n";
-        }
-        if ($dom = Crawler::getDomByNameAndClass($doc, 'div', 'a_k')) {
-            $content .= trim(Crawler::getTextFromDom($dom)) . "\n";
-        }
 
         $ret->body = trim($content);
         return $ret;
